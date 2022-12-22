d5c912a2c1548f7e0ff8e0361f93149838cd9982
it21028878 <it21028878@my.sliit.lk>
2022-12-22 19:57:45 +0530

    Another printf line

---
+++ 2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*stdio.h is a header file which contains information about standard input/output library 
+function calls such as printf.*/
+
+int main (void){
+
+  printf("Welcome to C!"); //output
+  printf("This is my first program"); //output
+
+  return 0 ;
+}
