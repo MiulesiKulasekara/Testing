80896ad555ab0419b7708013d3bf1ae3dccd1688
Miulesi Kulasekara <87386740+it21028878@users.noreply.github.com>
2022-12-22 21:34:27 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/*stdio.h is a header file which contains information about standard input/output library function calls such as printf.*/
+
+int main (void){
+
+  printf("Welcome to C!"); //output
+
+  return 0 ;
+}
